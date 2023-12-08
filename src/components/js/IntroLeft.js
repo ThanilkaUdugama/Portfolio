3d4e9f09f5e15c43c17f26dc62ed50e899a67f1b
ThanilkaUdugama <thanilkavishvajith@gmail.com>
2023-12-08 09:17:15 +0530

    replace image and add styling

--- src/components/js/IntroLeft.js
+++ src/components/js/IntroLeft.js
@@ -50,17 +50,17 @@ export default function IntroLeft(){
 
     return(
         <div className='bg-[#323445] w-full tablet:w-3/5 h-96 tablet:h-100 flex items-center justify-center relative'>
-                <img className="grayscale h-96 tablet:h-100 drop-shadow-2xl z-10" src = {User}></img>
+                <img className="grayscale saturate-50 h-96 tablet:h-100 drop-shadow-2xl z-10 contrast-80" src = {User}></img>
                 {/* <div className="absolute flex flex-col justify-center items-center bottom-10">
                     <span className="text-[#FEC158] text-6xl font-extrabold drop-shadow-2xl">{name1}</span>
                     <span className="text-[#FFFFFF] text-6xl font-extrabold drop-shadow-2xl">{name2}</span>
                 </div> */}
 
                 <div className="absolute z-20">
-                    <p className="text-white font-bold">{name}</p>
+                    <p className="text-white font-bold translate-y-4">{name}</p>
                 </div>
 
-                <div className=" absolute bg-[#FEC158] h-96 w-96 rounded-full">
+                <div className=" absolute bg-[#FEC158] mobile-s:h-64 mobile-s:w-64 mobile-m:w-72 mobile-m:h-72 mobile-l:h-80 mobile-l:w-80 laptop-m:h-96 laptop-m:w-96 translate-y-4 rounded-full">
                 </div>
 
         </div>
