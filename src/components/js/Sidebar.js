1b8ee54f3a274cbbc49c7edcfb6bfc8952281a24
ThanilkaUdugama <thanilkavishvajith@gmail.com>
2023-12-29 23:25:40 +0530

    done

--- src/components/js/Sidebar.js
+++ src/components/js/Sidebar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SidebarItem from './SidebarItem';
 export default function Sidebar({menu_func, activity, references}){
     return(
-        <div className='bg-[#323445] w-full z-10 fixed flex flex-col mt-14 pb-5 transition sidebar sidebar-closed tablet:hidden'>
+        <div className='bg-[#323445] w-full z-10 fixed flex flex-col mt-12 pb-5 transition sidebar sidebar-closed tablet:hidden'>
             <SidebarItem menu_func = {menu_func} tab_name="Introduction" selected={activity['intro']} reference = {references[0]}/>
             <SidebarItem menu_func = {menu_func} tab_name="Qualifications" selected={activity['qualifications']} reference = {references[1]}/>
             <SidebarItem menu_func = {menu_func} tab_name="Skills" selected={activity['skills']} reference = {references[2]}/>
--- src/components/js/SidebarItem.js
+++ src/components/js/SidebarItem.js
@@ -8,7 +8,7 @@ export default function SidebarItem({menu_func, tab_name, selected, reference}){
     }
 
     return(
-        <div className="flex justify-center items-center font-medium text-xl pb-4 pt-1 cursor-pointer" onClick={() => action()}>
+        <div className="flex justify-center items-center font-medium text-xl pb-4 pt-2 cursor-pointer" onClick={() => action()}>
             {(selected)? <div className="bg-[#FFD845] rounded-full w-2 h-2 mt-1 mr-4"></div> : <div className="w-1 h-1 mt-1"></div>}
             {(selected)? <a className="text-white">{tab_name}</a> :<a className="text-white opacity-50">{tab_name}</a>}
         </div>
